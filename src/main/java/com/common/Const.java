46a96169e632547c21b67eab68740a93fe5d178f
haominhnguyen <59317824+haominhnguyen@users.noreply.github.com>
2020-01-06 01:18:33 +0700

    hao fix file

--- src/main/java/com/common/Const.java
+++ src/main/java/com/common/Const.java
@@ -4,4 +4,5 @@ package com.common;
  * Created by haotr on 05/01/2020.
  */
 public class Const {
+    public static final String IPV4_PATTERN = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 }
--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.controller;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
@@ -13,6 +14,9 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,10 +38,16 @@ public class HomeController {
 
     @ResponseBody
     @RequestMapping(value = "/view_video", method = RequestMethod.POST)
-    public String viewVideo(HttpServletRequest request, @RequestParam("file") MultipartFile file) {
+    public String viewVideo(HttpServletRequest request, @RequestParam("file") MultipartFile file) throws IOException {
         String link = request.getParameter("link");
         String duration = request.getParameter("duration");
         System.out.println(file.getOriginalFilename());
+        String fileName = file.getOriginalFilename();
+        if (fileName.endsWith(".xlsx") || fileName.endsWith(".xls")) {
+            readFileExcel(file);
+        } else if (fileName.endsWith(".txt")) {
+            // TODO:
+        }
 
         try {
             sendPOST("http://localhost:8080//view_video_vps", link, duration);
@@ -68,7 +78,7 @@ public class HomeController {
         post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault();
-                CloseableHttpResponse response = httpClient.execute(post)) {
+             CloseableHttpResponse response = httpClient.execute(post)) {
             result = EntityUtils.toString(response.getEntity());
         }
 
@@ -80,4 +90,43 @@ public class HomeController {
         playVideo.start();
     }
 
+    public List<String> readFileExcel(MultipartFile file) {
+        List<String> listVPS = new ArrayList<>();
+        try {
+//            String pathDir = System.getProperty("user.dir");
+//            FileInputStream file = new FileInputStream(new File(pathDir + "/" + "data.xlsx"));
+            byte b[] = file.getBytes();
+            InputStream ips = new ByteArrayInputStream(b);
+            Workbook workbook = WorkbookFactory.create(ips);
+            //Create Workbook instance holding reference to .xlsx file
+            //Get first/desired sheet from the workbook
+            Sheet sheet = workbook.getSheetAt(0);
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext() && sheet.getRowSumsRight()) {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIP = row.cellIterator();
+
+                while (cellIP.hasNext()) {
+                    // next 1 element
+                    cellIP.next();
+                    Cell cell = cellIP.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue());
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue());
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listVPS;
+    }
 }
