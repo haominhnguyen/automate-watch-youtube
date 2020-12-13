84fe747e808dd8d87f84016fcdb0c9c24885df76
haominhnguyen <59317824+haominhnguyen@users.noreply.github.com>
2020-12-14 00:26:56 +0700

    delete unusse

--- src/main/java/com/controller/ChannelYoutubeController.java
+++ src/main/java/com/controller/ChannelYoutubeController.java
@@ -341,14 +341,6 @@ public class ChannelYoutubeController {
         System.out.println("Done: " + nameVideo);
     }
 
-    public static void main(String[] args) {
-        LocalDateTime myDateObj = LocalDateTime.now();
-        System.out.println("Before formatting: " + myDateObj);
-        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
-
-        String formattedDate = myDateObj.format(myFormatObj);
-        System.out.println("After formatting: " + formattedDate);
-    }
     /**
      * Convert Image to BufferedImage
      * @param img
